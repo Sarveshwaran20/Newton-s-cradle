d4429564b928d9b81886f4ce73eccccf451a7011
n-0064-12 <70128993+n-0064-12@users.noreply.github.com>
2020-10-14 21:06:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,69 +1,70 @@
 
-//var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-//var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const body = Matter.Body;
+const Render= Matter.Render;
 const Constraint = Matter.Constraint;
 
-var roof;
-var bob1;
-var rope1;
-function preload(){
-	
-}
 
 function setup() {
 	createCanvas(800, 400);
 
+
 	engine = Engine.create();
 	world = engine.world;
-  
-  //Create the Bodies Here.
-bob1 = new Bob(300,300);
-roof = new Roof(400,50,700,20);
-bob2 = new Bob(350,300);
-bob3 = new Bob(400,300);
-bob4 = new Bob(450,300);
-bob5 = new Bob(500,300);
-rope1 = new Rope(bob1.body,roof.body,-100,0);
-rope2 = new Rope(bob2.body,roof.body,-50,0);
-rope3 = new Rope(bob3.body,roof.body,0,0)
-rope4 = new Rope(bob4.body,roof.body,50,0)
-rope5 = new Rope(bob5.body,roof.body,100,0)
 
-	//Engine.run(engine);
+	//Create the Bodies Here.
+//suspension=createSprite(290,200,600,200);
+bob1=new Bob(300,350,50);
+bob2=new Bob(350,350,50);
+bob3=new Bob(400,350,50);
+bob4=new Bob(450,350,50);
+bob5=new Bob(500,350,50);
+//bob6=new Bob(150,250,30);
+ //fill ("brown");
+  ground1 = new Ground(400,100,300,40);
+  sling1= new Suspender(bob1.body,ground1.body,-50*2,0);
+  sling2= new Suspender(bob2.body,ground1.body,-25*2,0);
+  sling3= new Suspender(bob3.body,ground1.body,-0*2,0);
+  sling4= new Suspender(bob4.body,ground1.body,25*2,0);
+  sling5= new Suspender(bob5.body,ground1.body,50*2,0);
+  //sling6= new Suspender(bob6.body,ground1.body,-40*2,0);
+	Engine.run(engine);
   
 }
 
+
 function draw() {
+  background(100);
+//	ground.fill(48,22,8);
+	//ground.stroke(0);
+  rectMode(CENTER);
   Engine.update(engine);
-  background("lightgrey");
-  roof.display();
   bob1.display();
   bob2.display();
   bob3.display();
+  bob3.display();
   bob4.display();
   bob5.display();
-  rope1.display();
-  rope2.display();
-  rope3.display();
-  rope4.display();
-  rope5.display();
-  
+  //bob6.display();
+  ground1.display();
+  sling1.display();
+  sling2.display();
+  sling3.display();
+  sling4.display();
+  sling5.display();
+  //sling6.display();
+
+  keyPressed();
+  drawSprites ();
 }
 
-function keyPressed() {
-	if(keyCode === UP_ARROW){
-   
-		Matter.Body.applyForce(bob1.body,bob1.position, {x: -20, y: 10});
-		
-	}
-}
-function mouseDragged(){
-  Matter.Body.setPosition(bob1.body, {x: mouseX , y: mouseY});
-}
+function keyPressed() { 
+	if (keyCode === UP_ARROW) 
+	{ Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-100,y:-100}); } 
+} 
+
 
 
 
